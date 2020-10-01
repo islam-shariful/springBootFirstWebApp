4c68bef6a8a4b74bbdabfeb4b0f59274bb4ac785
shariful-islam-sharif <imdshariful171@gmail.com>
2020-10-02 01:17:26 +0600

    ModelAndView

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,22 +1,23 @@
 package com.example.demo;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	@RequestMapping("home")
 	//@ResponseBody
-	public String home(HttpServletRequest req, HttpServletResponse res) {
-		HttpSession session = req.getSession();
-		String name = req.getParameter("name");
-		System.out.println(name);
-		session.setAttribute("name", name);
-		return "home";
+	public ModelAndView home(@RequestParam("name") String myname) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myname);
+		mv.setViewName("home");
+		return mv;
 	}
 }
--- src/main/webapp/pages/home.jsp
+++ src/main/webapp/pages/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	Welcome to Spring Boot World, ${name}
+	Welcome to Spring Boot World, ${name} 
 </body>
 </html>
